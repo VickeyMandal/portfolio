67408db18d16be692959fc71d417dadd58415b09
VickeyMandal <mandalvickey@gmail.com>
2022-05-30 17:57:51 +0530

    add Analytics

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Timeline from "./components/Timeline";
 import Timeline2 from "./components/Timeline2";
+import ReactGA from "react-ga"
 
+ReactGA.initialize("G-0F5RBD5QG0");
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <div className="App">
